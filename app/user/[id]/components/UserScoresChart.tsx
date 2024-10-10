c7297e031597b96d17017fcf455b2a7e8bbb8eef
ItsRichardscull <totallynotrichardscull@gmail.com>
2024-10-10 09:00:09 +0100

    Use new graph endpoint with snapshots;

--- app/user/[id]/components/UserScoresChart.tsx
+++ app/user/[id]/components/UserScoresChart.tsx
@@ -1,4 +1,5 @@
 import { Score } from "@/lib/types/Score";
+import { StatsSnapshot } from "@/lib/types/StatsSnapshot";
 import { timeSince } from "@/lib/utils/timeSince";
 import {
   AreaChart,
@@ -12,95 +13,39 @@ import {
 
 interface Props {
   data: {
-    scores: Score[];
+    snapshots: StatsSnapshot[];
     total_count: number;
   };
 }
 
-// TODO: Will be reworked to use snapshots.
-// NOTE: Currently is unstable if user has more than 100 scores
-
-const bonusNumber = 416.6667;
-
-export default function UserScoresChart({ data }: Props) {
-  const scores = data.scores.sort((a, b) => {
-    return a.when_played.localeCompare(b.when_played);
+export default function UserStatsChart({ data }: Props) {
+  let snapshots = data.snapshots.sort((a, b) => {
+    return a.saved_at.localeCompare(b.saved_at);
   });
 
-  const perfomance = scores.map((s, i) => {
-    const scoresAtThatTime = scores
-      .slice(0, i + 1)
-      .sort((a, b) => b.performance_points - a.performance_points);
+  snapshots = snapshots.filter((s, i) => {
+    if (i === 0) {
+      return true;
+    }
 
-    let weightedPp = scoresAtThatTime
-      .map((s) => s.performance_points)
-      .reduce((acc, pp, index) => acc + Math.pow(0.95, index) * pp, 0);
+    const prevDate = new Date(snapshots[i - 1].saved_at);
+    const currentDate = new Date(s.saved_at);
 
-    let bonusPp = bonusNumber * (1 - Math.pow(0.9994, i));
-
-    return {
-      date: s.when_played,
-      pp: weightedPp + bonusPp,
-    };
+    return prevDate.toLocaleDateString() !== currentDate.toLocaleDateString();
   });
 
-  const perfomanceByDays = perfomance.reduce((acc, score) => {
-    const date = new Date(score.date).toLocaleDateString();
-
-    if (acc[date] && acc[date] > score.pp) {
-      return acc;
-    }
-
-    acc[date] = score.pp;
-    return acc;
-  }, {} as Record<string, number>);
-
-  const perfomanceByDaysMap = Object.entries(perfomanceByDays)
-    .map(([date, pp]) => ({
-      date,
-      pp,
-    }))
-    .sort((a, b) => {
-      return new Date(a.date).getTime() - new Date(b.date).getTime();
-    });
-
-  if (perfomanceByDaysMap.length === 0) {
-    return null;
-  }
-
-  const oldestDate = new Date(perfomanceByDaysMap[0].date);
-  const newestDate = new Date(new Date().toLocaleDateString());
-
-  const result = [];
-  let lastPP = null;
-  let currentDate = new Date(oldestDate);
-
-  while (currentDate <= newestDate) {
-    const currentDateStr = currentDate;
-
-    const existingEntry = perfomanceByDaysMap.find(
-      (d) => d.date === currentDateStr.toLocaleDateString()
-    );
-
-    if (existingEntry) {
-      lastPP = existingEntry.pp;
-      result.push({
-        date: timeSince(currentDate, true),
-        pp: lastPP,
-      });
-    } else if (lastPP !== null) {
-      result.push({
-        date: timeSince(currentDate, true),
-        pp: lastPP,
-      });
-    }
-
-    currentDate.setDate(currentDate.getDate() + 1);
-  }
+  const chartData = snapshots.map((s) => ({
+    date:
+      new Date(s.saved_at).getDate() === new Date().getDate()
+        ? "Today"
+        : timeSince(new Date(s.saved_at), true),
+    pp: s.pp,
+    rank: s.global_rank,
+  }));
 
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <AreaChart data={result}>
+      <AreaChart data={chartData}>
         <CartesianGrid stroke="" />
         <XAxis
           dataKey="date"
