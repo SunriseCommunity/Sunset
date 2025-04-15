b40c3354b142cd36e4201e6d7369d958ea811c46
richardscull <totallynotrichardscull@gmail.com>
2025-04-15 04:45:31 +0300

    feat: Force dark theme for brand in header;

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -43,7 +43,7 @@ export default function Header() {
           {/* Brand */}
           <a href="/" className="smooth-transition">
             <h1 className="text-4xl font-semibold smooth-transition pb-1 cursor-pointer flex flex-row">
-              <span className="text-primary">sun</span>
+              <span className="text-primary dark">sun</span>
               <span className="text-current">rise</span>
             </h1>
           </a>
