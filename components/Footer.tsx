a317e7bd7312c2fe55ebb581e48dfb091754a786
richardscull <totallynotrichardscull@gmail.com>
2025-04-15 16:04:53 +0300

    feat: Make footer more accessible on the phones

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,12 @@ import { BookCopy, Github, ServerCrash, UsersRoundIcon } from "lucide-react";
 export default async function Footer() {
   return (
     <footer className="border-t-2 text-current p-4 text-sm text-center space-y-4 bg-background/50 mt-8">
-      <div className="flex justify-center space-x-4">
+      <div className="md:flex grid justify-center space-x-4">
         <p>© 2024-2025 Sunrise Community</p>
         <p>•</p>
         <a
           href="https://github.com/SunriseCommunity"
-          className="flex items-center space-x-1 hover:text-primary smooth-transition cursor-pointer"
+          className="flex items-center justify-center space-x-1 hover:text-primary smooth-transition cursor-pointer"
         >
           <Github className="mr-1" />
           Source Code
@@ -16,7 +16,7 @@ export default async function Footer() {
         <p>•</p>
         <a
           href={`https://uptime.${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/`}
-          className="flex items-center space-x-1 hover:text-primary smooth-transition cursor-pointer"
+          className="flex items-center justify-center space-x-1 hover:text-primary smooth-transition cursor-pointer"
         >
           <ServerCrash className="mr-1" />
           Server Status
@@ -24,7 +24,7 @@ export default async function Footer() {
         <p>•</p>
         <a
           href={`https://api.${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/docs`}
-          className="flex items-center space-x-1 hover:text-primary smooth-transition cursor-pointer"
+          className="flex items-center justify-center space-x-1 hover:text-primary smooth-transition cursor-pointer"
         >
           <BookCopy className="mr-1" />
           API Docs
@@ -32,7 +32,7 @@ export default async function Footer() {
         <p>•</p>
         <a
           href={`https://discord.gg/BjV7c9VRfn`}
-          className="flex items-center space-x-1 hover:text-primary smooth-transition cursor-pointer"
+          className="flex items-center justify-center space-x-1 hover:text-primary smooth-transition cursor-pointer"
         >
           <UsersRoundIcon className="mr-1" />
           Discord server
