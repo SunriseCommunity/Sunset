8f56e4a74486783154ee70604a041bb36a03cc40
richardscull <totallynotrichardscull@gmail.com>
2025-04-15 16:43:39 +0300

    feat: Add vote button for osu-server-list in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,24 @@
-import { BookCopy, Github, ServerCrash, UsersRoundIcon } from "lucide-react";
+import {
+  BookCopy,
+  Github,
+  ServerCrash,
+  UsersRoundIcon,
+  VoteIcon,
+} from "lucide-react";
 
 export default async function Footer() {
   return (
-    <footer className="border-t-2 text-current p-4 text-sm text-center space-y-4 bg-background/50 mt-8">
+    <footer className="border-t-2 text-current p-4 text-sm text-center space-y-6 bg-background/50 mt-8">
+      <a
+        href={`https://osu-server-list.com/server/sunrise`}
+        className="flex items-center justify-center space-x-1 font-bold hover:scale-105  smooth-transition cursor-pointer "
+      >
+        <VoteIcon className="mr-1" />
+        <p className="from-stone-400 via-orange-300 to-amber-200 bg-gradient-to-r text-transparent bg-clip-text bg-size-300 animate-gradient ">
+          Please vote for us on osu-server-list!
+        </p>
+      </a>
+
       <div className="md:flex grid justify-center space-x-4">
         <p>© 2024-2025 Sunrise Community</p>
         <p>•</p>
