24d5376415e7f849bb3503fa2e5e6c27b902b17f
richardscull <totallynotrichardscull@gmail.com>
2025-04-16 17:02:19 +0300

    ref: Deprecate HeaderLoginDropdown; feat: Add HeaderLoginDialog element;

---
+++ components/Header/HeaderLoginDialog.tsx
@@ -0,0 +1,194 @@
+"use client";
+
+import React, { useContext, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DialogHeader,
+  DialogFooter,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import useSelf from "@/lib/hooks/useSelf";
+import useRestriction from "@/lib/hooks/useRestriction";
+import { useAuthorize } from "@/lib/hooks/api/auth/useAuthorize";
+
+import Cookies from "js-cookie";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useToast } from "@/hooks/use-toast";
+import { Separator } from "@/components/ui/separator";
+import { useRouter } from "next/navigation";
+import { MobileDrawerContext } from "@/components/Header/HeaderMobileDrawer";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, {
+      message: "Username must be at least 2 characters.",
+    })
+    .max(32, {
+      message: "Username must be 32 characters or fewer.",
+    }),
+  password: z
+    .string()
+    .min(8, {
+      message: "Password must be at least 8 characters.",
+    })
+    .max(32, {
+      message: "Password must be 32 characters or fewer.",
+    }),
+});
+
+export default function HeaderLoginDialog() {
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const { revalidate } = useSelf();
+  const { toast } = useToast();
+
+  const { setSelfRestricted } = useRestriction();
+  const { trigger: triggerAuthorize } = useAuthorize();
+
+  const setMobileDrawerOpen = useContext(MobileDrawerContext);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const { username, password } = values;
+
+    triggerAuthorize(
+      {
+        username,
+        password,
+      },
+      {
+        onSuccess(data) {
+          Cookies.set("session_token", data.token, {
+            expires: new Date(Date.now() + data.expires_in * 1000),
+          });
+
+          Cookies.set("refresh_token", data.refresh_token, {
+            expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+          });
+
+          revalidate();
+
+          toast({
+            title: "You succesfully logged in!",
+          });
+        },
+        onError(err) {
+          const errorMessage = err.message ?? "Unknown error";
+
+          if (errorMessage?.includes("restrict")) {
+            setSelfRestricted(true, errorMessage);
+            return;
+          }
+
+          setError(errorMessage || "Unknown error");
+        },
+      }
+    );
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">sign in</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Sign In To Proceed</DialogTitle>
+          <DialogDescription>Welcome back.</DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="e.g. username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="************"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {error && (
+              <p className="mx-auto text-sm text-destructive">{error}</p>
+            )}
+            <DialogFooter>
+              <Button
+                onClick={() => {
+                  MobileDrawerContext.Provider;
+                }}
+                type="submit"
+              >
+                Login
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+
+        <Separator className="my-2" />
+
+        <div className="flex flex-row justify-between ">
+          <DialogClose asChild>
+            <Button
+              variant="link"
+              onClick={() => {
+                router.push("/register");
+                setMobileDrawerOpen?.(false);
+              }}
+              className="w-full"
+            >
+              Don't have an account? Sign in
+            </Button>
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- components/Header/HeaderMobileDrawer.tsx
+++ components/Header/HeaderMobileDrawer.tsx
@@ -25,9 +25,16 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { Suspense } from "react";
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  Suspense,
+  useState,
+} from "react";
 import Image from "next/image";
 import { HeaderLogoutAlert } from "@/components/Header/HeaderLogoutAlert";
+import HeaderLoginDialog from "@/components/Header/HeaderLoginDialog";
 
 const navigationList = [
   {
@@ -52,105 +59,113 @@ const navigationList = [
   },
 ];
 
+export const MobileDrawerContext = createContext<Dispatch<
+  SetStateAction<boolean>
+> | null>(null);
+
 export default function HeaderMobileDrawer() {
+  const [open, setOpen] = useState(false);
+
   const { self } = useSelf();
 
   return (
-    <Drawer>
-      <DrawerTrigger>
-        <Menu />
-      </DrawerTrigger>
-      <DrawerContent className="p-4">
-        <DrawerHeader>
-          <DrawerTitle></DrawerTitle>
-          <div className="flex w-full items-center place-content-between">
-            {self ? (
-              <DrawerClose asChild>
-                <Link
-                  className="flex items-center flex-grow min-w-0"
-                  href={`/user/${self.user_id}`}
-                >
-                  <Avatar className="cursor-pointer smooth-transition">
-                    <Suspense fallback={<AvatarFallback>UA</AvatarFallback>}>
-                      <Image
-                        src={self.avatar_url}
-                        width={64}
-                        height={64}
-                        alt="Avatar"
-                      />
-                    </Suspense>
-                  </Avatar>
-                  <p className="block truncate mx-2 font-medium">
-                    {self.username}
-                  </p>
-                </Link>
-              </DrawerClose>
-            ) : (
-              <> {/* TODO: Sign in menu */}</>
-            )}
-            <div className="flex-shrink-0 scale-125 ">
-              <HeaderSearchCommand />
-              <ThemeModeToggle />
-            </div>
-          </div>
-        </DrawerHeader>
-
-        <DrawerFooter>
-          <ScrollArea className="h-72 rounded-md bg-transparent">
-            <div className="space-y-3 mb-8">
-              {self && (
-                <>
-                  <DrawerClose asChild>
-                    <Link
-                      href={`/user/${self.user_id}`}
-                      className="flex space-x-2"
-                    >
-                      <UserIcon />
-                      <p>Your profile</p>
-                    </Link>
-                  </DrawerClose>
-                  <Separator className="my-2" />
-                  <DrawerClose asChild>
-                    <Link href={`/friends`} className="flex space-x-2">
-                      <Users2 />
-                      <p>Friends</p>
-                    </Link>
-                  </DrawerClose>
-                  <Separator className="my-2" />
-                  <DrawerClose asChild>
-                    <Link href={`/settings`} className="flex space-x-2">
-                      <Cog />
-                      <p>Settings</p>
-                    </Link>
-                  </DrawerClose>
-                  <Separator className="my-2" />
-                  <HeaderLogoutAlert className="w-full">
-                    <div className="flex space-x-2">
-                      <DoorOpen />
-                      <p>Log out</p>
-                    </div>
-                  </HeaderLogoutAlert>
-                  <Separator className="my-2" />
-                </>
+    <MobileDrawerContext.Provider value={setOpen}>
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger>
+          <Menu />
+        </DrawerTrigger>
+        <DrawerContent className="p-4">
+          <DrawerHeader>
+            <DrawerTitle></DrawerTitle>
+            <div className="flex w-full items-center place-content-between">
+              {self ? (
+                <DrawerClose asChild>
+                  <Link
+                    className="flex items-center flex-grow min-w-0"
+                    href={`/user/${self.user_id}`}
+                  >
+                    <Avatar className="cursor-pointer smooth-transition">
+                      <Suspense fallback={<AvatarFallback>UA</AvatarFallback>}>
+                        <Image
+                          src={self.avatar_url}
+                          width={64}
+                          height={64}
+                          alt="Avatar"
+                        />
+                      </Suspense>
+                    </Avatar>
+                    <p className="block truncate mx-2 font-medium">
+                      {self.username}
+                    </p>
+                  </Link>
+                </DrawerClose>
+              ) : (
+                <HeaderLoginDialog />
               )}
+              <div className="flex-shrink-0 scale-125 ">
+                <HeaderSearchCommand />
+                <ThemeModeToggle />
+              </div>
             </div>
+          </DrawerHeader>
 
-            <div className="space-y-3">
-              {navigationList.map((tag) => (
-                <div key={tag.title}>
-                  <DrawerClose asChild>
-                    <Link href={tag.url} className="flex space-x-2">
-                      {tag.icon}
-                      <p>{tag.title}</p>
-                    </Link>
-                  </DrawerClose>
-                  <Separator className="my-2" />
-                </div>
-              ))}
-            </div>
-          </ScrollArea>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+          <DrawerFooter>
+            <ScrollArea className="h-72 rounded-md bg-transparent">
+              <div className="space-y-3 mb-8">
+                {self && (
+                  <>
+                    <DrawerClose asChild>
+                      <Link
+                        href={`/user/${self.user_id}`}
+                        className="flex space-x-2"
+                      >
+                        <UserIcon />
+                        <p>Your profile</p>
+                      </Link>
+                    </DrawerClose>
+                    <Separator className="my-2" />
+                    <DrawerClose asChild>
+                      <Link href={`/friends`} className="flex space-x-2">
+                        <Users2 />
+                        <p>Friends</p>
+                      </Link>
+                    </DrawerClose>
+                    <Separator className="my-2" />
+                    <DrawerClose asChild>
+                      <Link href={`/settings`} className="flex space-x-2">
+                        <Cog />
+                        <p>Settings</p>
+                      </Link>
+                    </DrawerClose>
+                    <Separator className="my-2" />
+                    <HeaderLogoutAlert className="w-full">
+                      <div className="flex space-x-2">
+                        <DoorOpen />
+                        <p>Log out</p>
+                      </div>
+                    </HeaderLogoutAlert>
+                    <Separator className="my-2" />
+                  </>
+                )}
+              </div>
+
+              <div className="space-y-3">
+                {navigationList.map((tag) => (
+                  <div key={tag.title}>
+                    <DrawerClose asChild>
+                      <Link href={tag.url} className="flex space-x-2">
+                        {tag.icon}
+                        <p>{tag.title}</p>
+                      </Link>
+                    </DrawerClose>
+                    <Separator className="my-2" />
+                  </div>
+                ))}
+              </div>
+            </ScrollArea>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </MobileDrawerContext.Provider>
   );
 }
