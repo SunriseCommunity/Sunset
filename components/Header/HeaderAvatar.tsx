70844694aece4e599c61027e175eccdb37e87fea
richardscull <totallynotrichardscull@gmail.com>
2025-04-16 17:02:19 +0300

    ref: Deprecate HeaderLoginDropdown; feat: Add HeaderLoginDialog element;

--- components/Header/HeaderAvatar.tsx
+++ components/Header/HeaderAvatar.tsx
@@ -1,34 +1,15 @@
 "use client";
 
-import { useState } from "react";
-import HeaderLoginDropdown from "@/components/Header/HeaderLoginDropdown";
-
 import useSelf from "@/lib/hooks/useSelf";
 import HeaderUserDropdown from "@/components/Header/HeaderUserDropdown";
+import HeaderLoginDialog from "@/components/Header/HeaderLoginDialog";
 
 export default function HeaderAvatar() {
-  const [isLoginDropdownOpen, setIsLoginDropdownOpen] = useState(false);
-
   const { self } = useSelf();
 
-  return (
-    <>
-      {self ? (
-        <HeaderUserDropdown self={self} />
-      ) : (
-        <div className="relative">
-          <button
-            onClick={() => setIsLoginDropdownOpen((prev) => !prev)}
-            className="hover:bg-zinc-600 p-1 rounded-md smooth-transition opacity-40 group-hover:opacity-100"
-          >
-            sign in
-          </button>
-          <HeaderLoginDropdown
-            isOpen={isLoginDropdownOpen}
-            setIsOpen={setIsLoginDropdownOpen}
-          />
-        </div>
-      )}
-    </>
+  return self ? (
+    <HeaderUserDropdown self={self} />
+  ) : (
+    <HeaderLoginDialog />
   );
 }
