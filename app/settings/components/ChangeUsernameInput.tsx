94fc99a71c1a1f0da97ea106cff3f0f1264b306b
richardscull <totallynotrichardscull@gmail.com>
2025-04-18 21:43:44 +0300

    feat: Update Password and Username Input to use zod form and shadecn Form element;

--- app/settings/components/ChangeUsernameInput.tsx
+++ app/settings/components/ChangeUsernameInput.tsx
@@ -1,32 +1,56 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { DialogFooter } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/hooks/use-toast";
 import { useUsernameChange } from "@/lib/hooks/api/user/useUsernameChange";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  newUsername: z
+    .string()
+    .min(2, {
+      message: "Username must be at least 2 characters.",
+    })
+    .max(32, {
+      message: "Username must be 32 characters or fewer.",
+    }),
+});
 
 export default function ChangeUsernameInput() {
   const [error, setError] = useState<string | null>(null);
 
+  const { toast } = useToast();
+
   const { trigger } = useUsernameChange();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      newUsername: "",
+    },
+  });
 
   const showError = (message: string) => {
     setError(message);
   };
 
-  const submitChangeUsernameForm = async (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
+  function onSubmit(values: z.infer<typeof formSchema>) {
     setError(null);
-    const form = e.currentTarget as HTMLFormElement;
 
-    const newUsername = form.newUsername.value;
-
-    if (newUsername.length < 2 || newUsername.length > 32) {
-      showError(
-        "Invalid new username Length should be between 2 and 32 characters."
-      );
-      return;
-    }
+    const { newUsername } = values;
 
     trigger(
       {
@@ -35,43 +59,48 @@ export default function ChangeUsernameInput() {
       {
         onSuccess: () => {
           form.reset();
-          alert("Username changed successfully!");
+
+          toast({ title: "Username changed successfully!" });
         },
         onError: (err) => {
           showError(err.message ?? "Unknown error.");
+          toast({
+            title: "Error occured while changing username!",
+            description: err.message ?? "Unknown error.",
+            variant: "destructive",
+          });
         },
       }
     );
-  };
+  }
 
   return (
     <div className="flex flex-col lg:w-1/2">
-      <form className="flex flex-col" onSubmit={submitChangeUsernameForm}>
-        <label htmlFor="username" className="text-gray-200 text-sm">
-          New Username
-        </label>
-        <input
-          type="text"
-          name="newUsername"
-          className="bg-terracotta-800 p-2 rounded-lg mb-2"
-          maxLength={32}
-          pattern="^[a-zA-Z0-9_\- ]{1,32}$"
-          required
-        />
-
-        {error && <p className="text-red-500">{error}</p>}
-
-        <button
-          type="submit"
-          className="bg-terracotta-500 text-white rounded-lg p-2"
-        >
-          Change Username
-        </button>
-        <label className="text-xs mt-2">
-          * Reminder: Please keep your username family friendly, or it will be
-          changed for you. Abusing this feature will result in a ban.
-        </label>
-      </form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="newUsername"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>New Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="e.g. username" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {error && <p className="text-sm text-destructive">{error}</p>}
+          <DialogFooter>
+            <Button type="submit">Change username</Button>
+          </DialogFooter>
+        </form>
+      </Form>
+      <label className="text-xs mt-2">
+        * Reminder: Please keep your username family friendly, or it will be
+        changed for you. Abusing this feature will result in a ban.
+      </label>
     </div>
   );
 }
