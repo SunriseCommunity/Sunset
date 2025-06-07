5747d40bcd5232565615347a3250e657e6c9e5e1
richardscull <106016833+richardscull@users.noreply.github.com>
2025-06-07 17:35:22 +0300

    feat: Add button for HeaderAvatr for better UX;

--- components/Header/HeaderAvatar.tsx
+++ components/Header/HeaderAvatar.tsx
@@ -6,17 +6,23 @@ import HeaderLoginDialog from "@/components/Header/HeaderLoginDialog";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Suspense } from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function HeaderAvatar() {
   const { self } = useSelf();
 
   return self ? (
     <HeaderUserDropdown self={self}>
-      <Avatar className="cursor-pointer smooth-transition hover:scale-110">
-        <Suspense fallback={<AvatarFallback>UA</AvatarFallback>}>
-          <Image src={self.avatar_url} width={64} height={64} alt="Avatar" />
-        </Suspense>
-      </Avatar>
+      <Button
+        variant="link"
+        className="bg-transparent p-0 rounded-full focus-visible:ring-0"
+      >
+        <Avatar className="cursor-pointer smooth-transition hover:scale-110">
+          <Suspense fallback={<AvatarFallback>UA</AvatarFallback>}>
+            <Image src={self.avatar_url} width={64} height={64} alt="Avatar" />
+          </Suspense>
+        </Avatar>
+      </Button>
     </HeaderUserDropdown>
   ) : (
     <HeaderLoginDialog />
