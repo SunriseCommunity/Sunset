07fb3363b1262e38f82a7d4906a0df0569e1ee01
richardscull <106016833+richardscull@users.noreply.github.com>
2025-12-20 22:53:15 +0200

    feat: Add context for locale messages

--- lib/i18n/request.ts
+++ lib/i18n/request.ts
@@ -3,15 +3,35 @@ import { cookies } from "next/headers";
 import defaultMessages from "./messages/en.json";
 import { merge } from "lodash";
 
+const hasContext = (value: any): value is { text: string; context: string } => {
+  return typeof value === "object" && "text" in value && "context" in value;
+};
+
+const extractTextFromMessages = (messages: any, result: any = {}) => {
+  Object.entries(messages).forEach(([key, value]) => {
+    if (typeof value === "string") {
+      result[key] = value;
+    } else if (hasContext(value)) {
+      result[key] = value.text;
+    } else {
+      result[key] = extractTextFromMessages(value);
+    }
+  });
+
+  return result;
+};
+
 export default getRequestConfig(async () => {
   const store = await cookies();
   const selectedLocale = store.get("locale")?.value || "en";
 
-  const messages = merge(
+  const rawMessages = merge(
     {},
     defaultMessages,
     (await import(`./messages/${selectedLocale}.json`)).default
   );
+
+  const messages = extractTextFromMessages(rawMessages);
 
   return {
     locale: selectedLocale,
