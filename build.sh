a27e3815b9e3f48ca76ba4bed5414c3a3ae99bc2
Fishia <74685931+fishiatee@users.noreply.github.com>
2026-03-08 14:09:21 +0700

    fix: actually copy everything to publish

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ npm run build
 mkdir publish
 
 cp run.sh publish/run.sh
-cp -r .next/standalone/* publish/*
+cp -r .next/standalone/* publish/
+cp -r .next/standalone/.next publish/.next
 cp -r public publish/public
 cp -r .next/static publish/.next/static
