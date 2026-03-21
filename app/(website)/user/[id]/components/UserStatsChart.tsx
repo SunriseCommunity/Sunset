7436c241a0c66f3a9ab45406c7ea6fd8e9e06339
richardscull <106016833+richardscull@users.noreply.github.com>
2026-03-21 23:44:30 +0200

    fix: Use variables from css styles instead hard coded one's for charts

--- app/(website)/user/[id]/components/UserStatsChart.tsx
+++ app/(website)/user/[id]/components/UserStatsChart.tsx
@@ -150,8 +150,8 @@ export default function UserStatsChart({ data, value: chartValue }: Props) {
         <Area
           type="monotone"
           dataKey={chartValue}
-          stroke="#E0C097"
-          fill="#E0C097"
+          stroke="hsl(var(--chart-1))"
+          fill="hsl(var(--chart-2))"
           baseValue={isChartReversed ? "dataMax" : "dataMin"}
           isAnimationActive={false}
         />
